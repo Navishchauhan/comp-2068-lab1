aff31daaca127b7129f67a602e7b34bb0bab779f
Navish <navishchauhan7@gmail.com>
2020-05-28 12:59:58 -0400

    completed javascript  lab1

--- lab-1.js
+++ lab-1.js
@@ -30,7 +30,14 @@ console.clear();
     Each object must have at least 2 properties.
 */
 
+ const perObj = [{ 
+    names: ["Kyle", "Adam", "Anders", "Blake"],
+	ages: [22, 23, 26, 23], 
+	hobbies: ["playing soccer", "playing hockey", "cooking", "gardening"],
+	programs: ["business", "fitness & health", "business", "nursing"] 
+}]; 
 
+console.log(perObj);
 /*
   Step 2 (6 points):
     Create an arrow function that accepts
@@ -40,7 +47,43 @@ console.clear();
     destructured.
 */
 
- 
+//Destructuring 
+const perNames = ["Kyle", "Adam", "Anders", "Blake"];
+const [person1, person2, person3, person4] = perNames;
+console.log(person1, person2, person3, person4);
+
+const perAges = [22, 23, 26, 23];
+const [age1, age2, age3, age4] = perAges;
+console.log(age1, age2, age3, age4);
+
+const perHobbies = ["playing soccer", "playing hockey", "cooking", "gardening"];
+const [hobby1, hobby2, hobby3, hobby4] = perHobbies;
+console.log(hobby1, hobby2, hobby3, hobby4);
+
+const perPrograms = ["business", "fitness & health", "business", "nursing"];
+const [program1, program2, program3, program4] = perPrograms ;
+console.log(program1, program2, program3, program4);
+
+//creating the arrow functions
+const firstPerson = () => {
+	console.log(`${person1}'s age is ${age1}. ${person1}'s hobby is ${hobby1} and ${person1} is studying ${program1}.`);
+};
+firstPerson();
+
+const secondPerson = () => {
+	console.log(`${person2}'s age is ${age2}. ${person2}'s hobby is ${hobby2} and ${person2} is studying ${program2}.`);
+};
+secondPerson();
+
+const thirdPerson = () => {
+	console.log(`${person3}'s age is ${age3}. ${person3}'s hobby is ${hobby3} and ${person3} is studying ${program3}.`);
+};
+thirdPerson();
+
+const fourthPerson = () => {
+	console.log(`${person4}'s age is ${age4}. ${person4}'s hobby is ${hobby4} and ${person4} is studying ${program4}.`);
+};
+fourthPerson();
 
 /*
   Step 3 (3 points):
@@ -48,3 +91,9 @@ console.clear();
     Call your arrow function passing the array item
     as an argument.
 */
+
+//looping the array using for/of loop
+for (let loop1 of perObj) {
+	console.log(loop1);
+}
+
